8183949cb4cd9627fe3f1c5bf193b89668037c95
Max Catman <xorgnak@gmail.com>
2024-07-11 16:16:05 -0600

    Update system.sh

--- system.sh
+++ system.sh
@@ -4,8 +4,8 @@ echo '$(screen -Dr || screen)' > /usr/bin/nomad
 
 chmod +x /usr/bin/nomad
 
-apt update
+#apt update
 
-apt upgrade -y
+#apt upgrade -y
 
 apt install -y sudo git screen ruby-full emacs-nox mosquitto mosquitto-clients
